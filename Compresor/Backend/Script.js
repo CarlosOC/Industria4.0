1ae7bbb296d427a242d4539fa47361b14d3301ea
Carlos Nicolas Oviedo Codigoni <carlosoviedolr@gmail.com>
2024-09-05 19:12:32 -0300

    Update Script.js

--- Compresor/Backend/Script.js
+++ Compresor/Backend/Script.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const moment = require('moment');
 const Datos = require('./WebScraping.js');
 let Data;
+let URLAPI="http://192.168.40.95:3010/compresor/atlas"
 let identificadorIntervaloDeTiempo = 60000
 async function main()
 {
@@ -23,7 +24,7 @@ async function Database(DataCompresor)
     DataCompresor.DataTimeData[1] = aux[2] + "/" + aux[1] + "/" + aux[0];
 
     try {
-        //const resultApi = await axios.put("http://192.168.40.95:3010/compresor/atlas", DataCompresor);
+        const resultApi = await axios.put(URLAPI, DataCompresor);
         console.log("Datos enviados a la API:", resultApi.data);
     } catch (error) {
         console.error("Error al enviar los datos a la API:", error);
